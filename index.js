793fcc9db1258c427f06785d513bc22b69464bd3
StasIpatov <166314021+StasIpatov@users.noreply.github.com>
2025-05-02 12:42:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// анімація при запуску сайту
+
+anime({
+    targets: 'body', 
+    scale: [0, 1], 
+    rotate: '1turn',
+    duration: 1000, 
+    delay: 50, 
+    easing: 'easeInOutQuad'  
+})
+
